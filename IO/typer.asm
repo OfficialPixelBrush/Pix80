9f572b950923a43b5e1848eba3cc664183cda188
OfficialPixelBrush <letsplaytvirtmann@gmail.com>
2022-01-26 21:04:04 +0100

    lots of stuff

---
+++ IO/typer.asm
@@ -0,0 +1,44 @@
+; I/O Device 0 is LCD Instructions (RS = 0)
+; I/O Device 1 is LCD Data         (RS = 1)
+; I/O Device 2 is Keyboard
+
+; Set DDRAM Address
+;LD A,0b10000000
+;:OUT (0),A
+
+keyboard:
+IN A,(2)         ; check keyboard buffer for new data
+; put PS/2 to ASCII Processing here
+CP 8             ; if the pressed key is 8, backspace has been pressed
+CALL Z,backspace ; enter backspace subroutine
+CP 32            ; if the Character is not printable ASCII
+JP C, keyboard   ; loop back to check for a new character
+INC D
+OUT (1),A        ; otherwise print the character that was found
+JP keyboard      ; then loop back to check again
+
+backspace:
+PUSH AF
+;DEC D            ; Decrement D
+CALL moveLCDtoD  ; Move Cursor to previous character 
+LD A, 32         ; print space over last character
+OUT (1),A        ; print space
+CALL moveLCDtoD  ; Move Cursor to previous character 
+POP AF           
+RET              ; return to function that called
+
+moveLCDtoD:
+; check if D is 13h or less
+LD A,D ; load D into A
+CP 14
+JP C, moveLCDend
+LD D,0
+; check if D is 14h - 27h, if yes, add 0x2C to A
+; check if D is 28h, make it 40h, vice versa too for 3F -> 27h
+; check if D is 40h  - 53h, if yes, subtract 0x2C from A
+; check if D is is >54h
+moveLCDend:
+LD A,0b10000000
+ADD A,D
+OUT (0),A
+RET
