c95f587406eceef6ac9894729fd60cf336c1fe67
Torben J. Virtmann <letsplaytvirtmann@gmail.com>
2022-03-23 13:34:12 +0100

    update

--- IO/printLCD.asm
+++ IO/printLCD.asm
@@ -55,41 +55,39 @@ OUT (0),A
 JR writeHelloWorld 
 exitHello:
 
-keyboard:
-IN A,(0)
+;keyboard:
+;IN A,(0)
 ; put PS/2 to ASCII Processing here v
-CP 8
-CALL Z,backspace
-CP 32 ; if the Character is within printable ASCII
-JP C, keyboard
-OUT (0),A ; then print the character
-OUT (3),A
-JP keyboard
+;CP 8
+;CALL Z,backspace
+;CP 32 ; if the Character is within printable ASCII
+;JP C, keyboard
+;OUT (0),A ; then print the character
+;OUT (3),A
+;JP keyboard
 
 backspace:
-PUSH AF
+;PUSH AF
 ; clear char
-LD A, 0b00010000
-OUT (1),A ; reverse printing direction to r-l
+;LD A, 0b00010000
+;OUT (1),A ; reverse printing direction to r-l
 
-LD A, 32
-OUT (0),A ; print space
+;LD A, 32
+;OUT (0),A ; print space
 
 ; clear char
-LD A, 0b00010000
-OUT (1),A ; reverse printing direction to r-l
+;LD A, 0b00010000
+;OUT (1),A ; reverse printing direction to r-l
 
-POP AF
-RET
-
-
+;POP AF
+;RET
 ; LCD Printing handling
 ;INC E ; increase Display Position
 ; insert handling of lines here
 ;LD A,0b10000000
 ;ADD A,E ; Set display Address 
 ;OUT (1),A   ; Output on Data Bus
-JP keyboard  ; return to keyboard loop
+;JP keyboard  ; return to keyboard loop
 
 hello:
     DEFM  " Hello, world! ",0
