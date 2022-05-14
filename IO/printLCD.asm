1eaf21ef74d01010d6ce94918de8faee585c3147
OfficialPixelBrush <letsplaytvirtmann@gmail.com>
2022-05-14 21:18:16 +0200

    f

--- IO/printLCD.asm
+++ IO/printLCD.asm
@@ -3,57 +3,44 @@
 ; I/O Device 2 is Keyboard
 ; Enable Display
 LD A,0b00001100
-OUT (1),A
+OUT (0),A
 
 ; Set CGRAM address
-LD A,0b01000000
-OUT (1),A
+;LD A,0b01000000
+;OUT (0),A
 
 ; Write Data to Screen
 ; Last 3 bits shouldn't matter
-LD A,0b00000
-OUT (0),A
-LD A,0b01010
-OUT (0),A
-LD A,0b01010
-OUT (0),A
-LD A,0b00000
-OUT (0),A
-LD A,0b11111
-OUT (0),A
-LD A,0b01110
-OUT (0),A
-LD A,0b00000
-OUT (0),A
-OUT (0),A
+;LD A,0b00000
+;OUT (1),A
+;LD A,0b01010
+;OUT (1),A
+;LD A,0b01010
+;OUT (1),A
+;LD A,0b00000
+;OUT (1),A
+;LD A,0b11111
+;OUT (1),A
+;LD A,0b01110
+;OUT (1),A
+;LD A,0b00000
+;OUT (1),A
+;OUT (1),A
 ; Should be smiley
 
 ; Set DDRAM Address
 LD A,0b10000000
-OUT (1),A
+OUT (0),A
 
 ; Print Character
-LD A,0
-OUT (0),A
-
-; test
-IN A,(3)
-LD HL, 0x8000 
-LD (HL),A  
+;LD A,0
+;OUT (1),A
  
 ; IX used as pointer
-LD IX,0
-writeHelloWorld:
-; hello offset + pointer
-LD A,(IX+hello)
-; increment IX
-INC IX
-CP 0
-JP Z,exitHello
-; output on data bus to LCD
-OUT (0),A
-JR writeHelloWorld 
-exitHello:
+LD HL,hello
+LD C,1
+LD B,afterhello-hello
+OTIR
 
 ;keyboard:
 ;IN A,(0)
@@ -90,7 +77,8 @@ backspace:
 ;JP keyboard  ; return to keyboard loop
 
 hello:
-    DEFM  " Hello, world! ",0
+    DEFM  "Hello, world! "
+afterhello:
 
 panda:
     ;INCLUDE "redPanda.bin"
